e5e4ec947a28db98f6c01e0b3efcf576e35c6ff3
taranvbi <taranavabbi@gmail.com>
2025-04-01 01:42:37 +0330

    Add files via upload

---
+++ T8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, i;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            for (i = a + 1; i <= b - 1; i++)
+            {
+                if (i % 5 == 0)
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}

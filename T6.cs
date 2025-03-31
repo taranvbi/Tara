9abf993262a66a3b763b794a5835322825c0e213
taranvbi <taranavabbi@gmail.com>
2025-04-01 01:41:22 +0330

    Add files via upload

---
+++ T6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, num;
+            a = 0;
+            num = int.Parse(Console.ReadLine());
+            if (num < 10)
+            { a = num; }
+            else if (num < 100)
+            { a = num / 10; }
+            else if (num < 1000)
+            { a = num / 100; }
+            else if (num < 10000)
+            { a = num / 1000; }
+            else if (num < 100000)
+            { a = num / 10000; }
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}

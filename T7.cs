23099eeb6eb3a542fdc5375447b89e388c3d89bd
taranvbi <taranavabbi@gmail.com>
2025-04-01 01:41:56 +0330

    Add files via upload

---
+++ T7.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, s = 0;
+            char r;
+            a = int.Parse(Console.ReadLine());
+            r = Console.ReadKey().KeyChar;
+            b = int.Parse(Console.ReadLine());
+            if (r == '*')
+                s = a * b;
+            else if (r == '/')
+                s = a / b;
+            else if (r == '+')
+                s = a + b;
+            else if (r == '-')
+                s = a - b;
+            Console.WriteLine("s=" + s);
+            Console.ReadLine();
+        }
+    }
+}
